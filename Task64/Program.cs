83d7aba2e2d924a9449ba99b72811befd3861f0d
Igor <kugimn@list.ru>
2022-10-15 00:03:54 +0300

    64 66 +

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+ Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+void PrintRange(int N)
+{
+    if (N != 0)
+    {
+        Console.Write($"{N}, ");
+        PrintRange(N - 1);
+    }
+}
+
+Console.WriteLine("Введите число N");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+PrintRange(userNumber);
---
+++ Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumFromMToN(int M, int N)
+{
+    int sum = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        M++;
+        sum = M + SumFromMToN(M, N);
+        return sum;
+    }
+}
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+ Console.Write(SumFromMToN(M - 1, N));
+
